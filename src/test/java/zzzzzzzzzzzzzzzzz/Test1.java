8aaa39ee0740297a21e02a436ab16d5b49abf116
QSP <QSP@QSP>
2021-06-08 07:54:28 +0530

    Hello

---
+++ src/test/java/zzzzzzzzzzzzzzzzz/Test1.java
@@ -0,0 +1,12 @@
+package zzzzzzzzzzzzzzzzz;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void tc1() {
+		Reporter.log("hello selenium",true);
+	}
+
+}
