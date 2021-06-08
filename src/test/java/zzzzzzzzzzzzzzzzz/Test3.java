f0230d0e5fbabb27783cf1dc251a2bdd8076cda6
QSP <QSP@QSP>
2021-06-08 08:09:08 +0530

    Hi

--- src/test/java/zzzzzzzzzzzzzzzzz/Test3.java
+++ src/test/java/zzzzzzzzzzzzzzzzz/Test3.java
@@ -3,10 +3,10 @@ package zzzzzzzzzzzzzzzzz;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class Test3 {
 	@Test
 	public void tc1() {
-		Reporter.log("hello Python",true);
+		Reporter.log("hello selenium",true);
 	}
 
 }
--- src/test/java/zzzzzzzzzzzzzzzzz/Test4.java
+++ src/test/java/zzzzzzzzzzzzzzzzz/Test4.java
@@ -3,10 +3,10 @@ package zzzzzzzzzzzzzzzzz;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test1 {
+public class Test4 {
 	@Test
 	public void tc1() {
-		Reporter.log("hello Python",true);
+		Reporter.log("hello Appium",true);
 	}
 
 }
